34877712255111db0ff655997893bb5976dac4b0
Dan Koenig <dan.koenig@helloworld.com>
2020-12-06 18:14:49 -0500

    restored

--- scripts/tiltTracker.js
+++ scripts/tiltTracker.js
@@ -26,20 +26,30 @@ let top;
 let center;
 let proj;
 let rectTop = 0;
+let moverPos = {x: 0, y:0};
+
+let size = {w: 100, h: 50};
+let hitters = [{x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}];
+let hits = [{h: false, v: false}];
+
+let times = [
+    {current: 0, last: 0, running:true, div: 2, startx: 0, endx: 0},
+    {current: 0, last: 0, running:true, div: 2, startx: 0, endx: 0},
+    {current: 0, last: 0, running:true, div: 2, startx: 0, endx: 0}
+];
 
 Scene.root.findFirst('canvas0').then(function (r) {
     const canvasBounds = r.bounds;
     top = canvasBounds.height.mul(.75);
     center = canvasBounds.width.mul(.5);
-    Patches.inputs.setScalar('top', top);
     Patches.inputs.setScalar('center', center);
     Scene.root.findFirst('plane0').then(function (r) {
-        r.worldTransform.position.x.monitor().subscribe(function (z) {
+        r.worldTransform.position.x.monitor().subscribe(function (x) {
             
             // Diagnostics.log(z.newValue);
-            tilt = z.newValue;
+            tilt = x.newValue;
             // Diagnostics.log(tilt);
-            Patches.inputs.setScalar('tilt', (tilt * -1000) - 50);
+            Patches.inputs.setScalar('tilt', (tilt * -1000) - 120);
             
             // if(rect){
             //     rect.transform.position.x = z.newValue;
@@ -51,18 +61,58 @@ Scene.root.findFirst('canvas0').then(function (r) {
     //     // const rect = n;
         
     // });
-    const units = 2;
     Scene.root.findFirst('timeTracker').then(function (r) {
         r.worldTransform.position.x.monitor().subscribe(function (x) {
             let oldRectTop = rectTop;
-            rectTop = Math.abs(x.newValue)%units;
-            if(rectTop < oldRectTop){
-                Patches.inputs.setScalar('horizontal', canvasBounds.width.mul(Math.random()));
+            // times[0] = x.newValue;'
+            if(times[0].running){
+                times[0].current += x.newValue - times[0].last;
             }
-            proj = canvasBounds.height.div(units).mul(rectTop);
-            
-            Patches.inputs.setScalar('projectile', proj);
+            times[0].last = x.newValue;
+            rectTop = Math.abs(x.newValue)%times[0].div;
+            if(rectTop < oldRectTop){
+                // Diagnostics.log('reastarted');
+                times[0].startx = Math.random();
+                times[0].endx = Math.random();
+                Patches.inputs.setScalar('horizontal1', canvasBounds.width.mul(Math.random()));
+                
+                // times[0].running = Math.random() > .1;
+            }
+            proj = canvasBounds.height.div(times[0].div).mul(Math.abs(times[0].current)%times[0].div);
+
+            // let mult = (((times[0].startx - times[0].endx) * times[0].current%times[0].div) / times[0].div);
+            // Diagnostics.log(mult);
+            // Patches.inputs.setScalar('horizontal1', canvasBounds.width.mul(mult));
+            Patches.inputs.setScalar('vertical1', proj);
+            // Diagnostics.log('running');
         });
+    });
+
+    Scene.root.findFirst('rectangle0').then(function (r0) {
+        r0.transform.position.x.monitor().subscribe(function (x) {
+            moverPos.x = x.newValue;
+        });
+        r0.transform.position.y.monitor().subscribe(function (y) {
+            moverPos.y = y.newValue;
+        });
+        Scene.root.findFirst('rectangle1').then(function (r1) {
+            Patches.inputs.setScalar('top', top);
+            r1.transform.position.x.monitor().subscribe(function (x) {
+                hitters[0].x = x.newValue;
+            });
+            r1.transform.position.y.monitor().subscribe(function (y) {
+                hitters[0].y = y.newValue;
+                hits[0].h = hitters[0].x > moverPos.x && hitters[0].x < moverPos.x + size.w;
+                hits[0].v = hitters[0].y > moverPos.y && hitters[0].y < moverPos.y + size.h;
+                // Diagnostics.log(moverPos.y);
+                if(hits[0].h && hits[0].v){
+                    Diagnostics.log('HIT!!');
+                }
+                
+                
+            });
+        });
+
     });
     
 
